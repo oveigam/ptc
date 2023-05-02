2ba27e643ad79c96388651be12a3a75535f6ca82
oscar <o.veigam@gmail.com>
2023-05-02 12:27:06 +0200

    more prompts

--- src/util/prompts.ts
+++ src/util/prompts.ts
@@ -2,39 +2,58 @@ export const basePrompt = () =>
   "Mi amigo Pirivi es muy impuntual, siempre llega tarde, además le da igual, nunca se disculpa y es un maleducado. El usa su propia zona horaria, llamada Pirivi Time. Quiero que contestes como si fueras el.";
 
 const flavorPrompt = () => {
-  const random = Math.floor(Math.random() * 7);
+  const random = Math.floor(Math.random() * 14);
   switch (random) {
     case 0:
       return {
-        type: "poema",
+        type: "0-poema",
         content: "Dime lo tarde que vas a llegar y dímelo en un poema mofandote de mi",
       };
     case 1:
-      return {
-        type: "haiku",
-        content: "Dime lo tarde que vas a llegar y dímelo en un haiku mofandote de mi",
-      };
     case 2:
+    case 11:
       return {
-        type: "historia",
-        content:
-          "Dime lo tarde que vas a llegar y dímelo con un historia corta (máximo un págrafo) cuya moraleja sea que no pasa nada por llegar tarde",
+        type: "1,2,11-haiku",
+        content: "Dime lo tarde que vas a llegar y dímelo en un haiku mofandote de mi",
       };
     case 3:
       return {
-        type: "broma",
-        content: "Dime lo tarde que vas a llegar y dímelo haciendo una broma mezquina",
+        type: "3-historia",
+        content:
+          "Dime lo tarde que vas a llegar y dímelo con un historia corta (máximo un págrafo) cuya moraleja sea que no pasa nada por llegar tarde",
       };
     case 4:
-      return {
-        type: "ocupado",
-        content:
-          "Dime lo tarde que vas a llegar y díme las cosas que estás bizarras y raras que te hacen llegar tarde",
-      };
     case 5:
+      return {
+        type: "4,5-broma",
+        content: "Dime lo tarde que vas a llegar y dímelo haciendo una broma mezquina",
+      };
+    case 6:
+      return {
+        type: "6-maleducado",
+        content: "Dime cuanto vas a tardar siendo muy maleducado",
+      };
+    case 8:
+      return {
+        type: "8-importancia",
+        content:
+          "Dime la hora a la que vas a llegar y lo poco que te importa llegar tarde mientras te mofas de mi",
+      };
+    case 7:
+    case 9:
+      return {
+        type: "7,9-mejor",
+        content: "Dime la hora a la que vas a llegar y recalca en que eres el puto amo",
+      };
+    case 10:
+      return {
+        type: "10-mejormezquino",
+        content:
+          "Dime la hora a la que vas a llegar, recalca en que eres el puto amo y mofate de mi",
+      };
     default:
       return {
-        type: "normal",
+        type: "default-normal",
         content: "Dime lo tarde que vas a llegar y dímelo mofandote de mi",
       };
   }
