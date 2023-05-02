90e39a7dc1be3cc528e635db82efde91b80c5608
oscar <o.veigam@gmail.com>
2023-05-02 11:04:34 +0200

    move to edge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import { Time } from "@ptc/app/Time";
 import { Form } from "./Form";
 
 import openai from "./openai.svg";
+import { ServerRuntime } from "next";
+
+export const runtime: ServerRuntime = "edge";
 
 export default function Home() {
   return (
@@ -11,9 +14,9 @@ export default function Home() {
         <h1 className="bg-gradient-to-l from-pink-400 to-slate-600 bg-clip-text text-center text-4xl font-semibold text-transparent lg:text-7xl">
           Pirivi Time Converter
         </h1>
-        <div className="flex justify-end items-center gap-1">
+        <div className="flex items-center justify-end gap-1">
           <h2 className="text-end font-light">powered by OpenAI</h2>
-          <Image src={openai} className="w-4 h-4" alt="openai" />
+          <Image src={openai} className="h-4 w-4" alt="openai" />
         </div>
       </div>
       <Time />
